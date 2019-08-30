d1df8e1d052aa1e9e996ae507164e1fdf7c7c113
Viktor-Borg <54361168+Viktor-Borg@users.noreply.github.com>
2019-08-31 00:18:22 +0200

    Baseclass

---
+++ Gymnasiearbete/Assets/Scripts/Templates/Weapons/WeaponPartTemplate.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+
+namespace ArenaShooter.Templates.Weapons
+{
+    abstract class WeaponPartTemplate : ScriptableObject
+    {
+        [SerializeField]private ushort templateId;
+        [SerializeField]private string name;
+
+        [TextArea]
+        [SerializeField]private string description;
+
+        public ushort TemplateId
+        {
+            get
+            {
+                return templateId;
+            }
+        }
+        
+    }
+}
+
+
