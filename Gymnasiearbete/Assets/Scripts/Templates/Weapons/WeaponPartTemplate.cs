3627c2ef21ffcfbdf84a07376f3136befc07b6ee
Viktor-Borg <54361168+Viktor-Borg@users.noreply.github.com>
2019-08-31 12:51:47 +0200

    WeaponPartTemplate first edition done

--- Gymnasiearbete/Assets/Scripts/Templates/Weapons/WeaponPartTemplate.cs
+++ Gymnasiearbete/Assets/Scripts/Templates/Weapons/WeaponPartTemplate.cs
@@ -3,13 +3,26 @@
 
 namespace ArenaShooter.Templates.Weapons
 {
+    enum WeaponTemplateType
+    {
+        stock,
+        body,
+        barrel
+    }
     abstract class WeaponPartTemplate : ScriptableObject
     {
+        [Header("General")]
         [SerializeField]private ushort templateId;
         [SerializeField]private string name;
+        
 
         [TextArea]
         [SerializeField]private string description;
+        
+        [Header("Graphics")]
+        [SerializeField]private GameObject weaponPartPrefab;
+
+        public abstract WeaponTemplateType type { get; }
 
         public ushort TemplateId
         {
@@ -18,6 +31,9 @@ namespace ArenaShooter.Templates.Weapons
                 return templateId;
             }
         }
+
+        
+
         
     }
 }
