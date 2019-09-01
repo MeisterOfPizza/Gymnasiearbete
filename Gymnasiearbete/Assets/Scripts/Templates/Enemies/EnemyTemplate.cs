be6cddc14a3d353b0695ea99d41280966916c4e4
Viktor-Borg <54361168+Viktor-Borg@users.noreply.github.com>
2019-09-01 15:25:08 +0200

    EnemyTemplate done

---
+++ Gymnasiearbete/Assets/Scripts/Templates/Enemies/EnemyTemplate.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ArenaShooter.Templates.Enemies
+{
+    enum AttackType
+    {
+        Raycast,
+        Projectile,
+        Electric,
+        Support,
+        Melee
+    }
+    [CreateAssetMenu(menuName = "Templates/Enemies/Enemy")]
+    class EnemyTemplate : ScriptableObject
+    {
+        #region inputs
+        [Header("Inputs")]
+        [SerializeField] private ushort id;
+        [SerializeField] private string nameEnemy;
+        [SerializeField] private GameObject prefab;
+        [SerializeField] private short health;
+        [SerializeField] private float movmentSpeed;
+        [SerializeField] private float engageRange;
+        #endregion
+
+        #region getters
+        public ushort Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+
+        public string NameEnemy
+        {
+            get
+            {
+                return nameEnemy;
+            }
+        }
+
+        public GameObject Prefab
+        {
+            get
+            {
+                return prefab;
+            }
+        }
+
+        public short Health
+        {
+            get
+            {
+                return health;
+            }
+        }
+
+        public float MovementSpeed
+        {
+            get
+            {
+                return movmentSpeed;
+            }
+        }
+
+        public float EngageRange
+        {
+            get
+            {
+                return engageRange;
+            }
+        }
+        #endregion
+    }
+}
+    
+
+
