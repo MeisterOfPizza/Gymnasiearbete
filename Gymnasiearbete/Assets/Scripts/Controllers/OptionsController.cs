70b02b248b844689d884fffc7490af56b2d5d5b7
Viktor-Borg <54361168+Viktor-Borg@users.noreply.github.com>
2019-09-11 10:48:27 +0200

    First Draft
    Everything is workin as expected. Except for the audiMixers. Their event systems are set up but the methods are currently empty.

---
+++ Gymnasiearbete/Assets/Scripts/Controllers/OptionsController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace ArenaShooter.Controllers
+{
+
+    class OptionsController : Controller<OptionsController>
+    {
+
+        #region Editor
+
+        [SerializeField] private TMPro.TMP_Dropdown resolutionsDropDown;
+        [SerializeField] private AudioMixer master;
+        [SerializeField] private AudioMixer SFX;
+        [SerializeField] private AudioMixer music;
+        [SerializeField] private AudioMixer misc;
+
+        #endregion
+
+        #region PrivateVariables
+
+        Resolution[] resolutions;
+
+        #endregion
+
+        #region Methods
+
+        private void Start()
+        {
+            QualitySettings.SetQualityLevel(3);
+            resolutions = Screen.resolutions;
+            resolutionsDropDown.ClearOptions();
+
+            List<string> options = new List<string>();
+
+            int currenResolutionsIndex = 0;
+            for (int i = 0; i < resolutions.Length; i++)
+            {
+                options.Add(resolutions[i].width + " x " + resolutions[i].height);
+
+                if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+                {
+                    currenResolutionsIndex = i;
+                }
+            }
+
+            resolutionsDropDown.AddOptions(options);
+            resolutionsDropDown.value = currenResolutionsIndex;
+            resolutionsDropDown.RefreshShownValue();
+        }
+
+        public void SetGraphicsQuality(int qualityIndex)
+        {
+            QualitySettings.SetQualityLevel(qualityIndex);
+        }
+
+        public void SetShadowQuality(int qualityIndex)
+        {
+            ShadowResolution shadows = (ShadowResolution)qualityIndex;
+        }
+
+        public void FullScreen(bool isFullScreen)
+        {
+            Screen.fullScreen = isFullScreen;
+        }
+
+        public void SetResolution(int resolutionIndex)
+        {
+            Resolution resolution = resolutions[resolutionIndex];
+            Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        }
+
+        #endregion
+
+        #region AudioMixerMethods
+
+        public void SetMasterVolume(float volume)
+        {
+            Debug.Log(volume);
+        }
+
+        public void SetMusicVolume(float volume)
+        {
+            Debug.Log(volume);
+        }
+
+        public void SetSFXVolume(float volume)
+        {
+            Debug.Log(volume);
+        }
+
+        public void SetMiscVolume(float volume)
+        {
+            Debug.Log(volume);
+        }
+
+        #endregion
+
+    }
+
+}
+
